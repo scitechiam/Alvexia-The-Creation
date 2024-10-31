1479c2ad896228252d6c4caf03fa6407ef6f754d
scitechiam <scitech.iam@gmail.com>
2024-10-31 12:37:28 -0500

    feat (mongodb): added mongo db connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
-const {
-  SERVER_PORT
-} = require('../config');
+const { CLIENT, SERVER_PORT } = require("../config");
 const connectDB = require("./database/connection");
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const router = require("./routes/router");
 const verifyToken = require("./routes/auth/verifyToken.js").verifySocketToken;
 
@@ -16,24 +14,24 @@ app.use(express.json());
 connectDB();
 
 app.use(router);
-/*El Tarvi agrego esto*/
-app.use(express.static(CLIENT + '/public'));
+
+app.use(express.static(CLIENT + "/public"));
 
 module.exports = io;
 const sock = require("./engine/socket.js");
 
 io.users = {};
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   // Leer el token desde el handshake
   const token = socket.handshake.query.token;
 
   if (!token) {
-    socket.emit('error', 'NO_TOKEN');
+    socket.emit("error", "NO_TOKEN");
     return socket.disconnect();
   }
 
   const authTimeout = setTimeout(() => {
-    socket.emit('error', 'AUTHENTICATION_TIMEOUT');
+    socket.emit("error", "AUTHENTICATION_TIMEOUT");
     socket.disconnect();
   }, 3000); // 3 segundos para autenticar
 
@@ -41,23 +39,23 @@ io.on('connection', (socket) => {
   const verifiedToken = verifyToken(token);
 
   if (!verifiedToken.valid) {
-    socket.emit('error', 'INVALID_TOKEN');
+    socket.emit("error", "INVALID_TOKEN");
     return socket.disconnect();
   }
 
   clearTimeout(authTimeout);
-  socket.emit('authenticated', 'User authenticated successfully');
+  socket.emit("authenticated", "User authenticated successfully");
   socket.user_id = verifiedToken.user.id;
   io.users[socket.user_id] = socket;
   sock(socket, io);
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
     clearTimeout(authTimeout);
   });
 });
 
 // Iniciar servidor
 server.listen(SERVER_PORT, () => {
-  console.log('Server running ' + SERVER_PORT);
+  console.log("Server running " + SERVER_PORT);
 });
